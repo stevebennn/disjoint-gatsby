0e0f6a837e565364cda4033d7d35c08c17a58fc3
stephen bennett <stephenbennett@stephens-MacBook-Pro.local>
2020-05-12 14:28:34 -0400

    adding PLP behavior

--- src/components/modules/search/search.js
+++ src/components/modules/search/search.js
@@ -3,12 +3,9 @@ import { Link } from 'gatsby'
 
 // Search component
 class Search extends Component {
-  constructor() {
-    super()
-    state = {
-      query: '',
-      results: [],
-    }
+  state = {
+    query: '',
+    results: [],
   }
 
   render() {
