8284e49536203aae77a14f93c280e7f1d1ca6437
stephen bennett <stephenbennett@stephens-MacBook-Pro.local>
2020-05-15 14:44:01 -0400

    trying to fix the build for /styleguide

--- src/pages/styleguide.js
+++ src/pages/styleguide.js
@@ -1,19 +1,22 @@
 import React from 'react'
 import Colors from '../styleguide/colors'
 import Type from '../styleguide/type'
+import Layout from '../components/layout'
 
 function Styleguide() {
   return (
-    <div className="w-full max-w-4xl m-auto ">
-      <h2 className="text-2xl w-full p-3 text-bold border-b-2 mb-10 ">
-        Typography
-      </h2>
-      <Type />
-      <h2 className="text-2xl w-full p-3 my-10 font-bold border-b-2 ">
-        Colors
-      </h2>
-      <Colors />
-    </div>
+    <Layout>
+      <div className="w-full max-w-4xl m-auto ">
+        <h2 className="text-2xl w-full p-3 text-bold border-b-2 mb-10 ">
+          Typography
+        </h2>
+        <Type />
+        <h2 className="text-2xl w-full p-3 my-10 font-bold border-b-2 ">
+          Colors
+        </h2>
+        <Colors />
+      </div>
+    </Layout>
   )
 }
 
